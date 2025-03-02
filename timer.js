d2c0d142223d01543532e2e224d212cbae021219
OVERCATALYST <143509672+OVERCATALYST@users.noreply.github.com>
2025-03-02 17:51:01 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,23 @@
+const eventDate = new Date("March 28, 2025 00:00:00").getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const timeLeft = eventDate - now;
+
+    if (timeLeft > 0) {
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours.toString().padStart(2, "0");
+        document.getElementById("minutes").innerText = minutes.toString().padStart(2, "0");
+        document.getElementById("seconds").innerText = seconds.toString().padStart(2, "0");
+    } else {
+        document.querySelector(".countdown-box").innerHTML = `<span style="font-size:2rem;">Event Started</span>`;
+    }
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
